899d3812b7aebecdf86e6338cf3f7e82c99e0742
Nameless <50556449+Mikami91@users.noreply.github.com>
2020-05-20 23:47:20 -0400

    First steps...

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // final wordPair = WordPair.random();
+    return MaterialApp(
+      title: 'FlutterNess',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Dark Souls'),
+        ),
+        body: Center(
+          // child: Text(wordPair.asPascalCase),
+          child: RandomWords(),        
+        ),
+      ),
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => new RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
+  }
+}
